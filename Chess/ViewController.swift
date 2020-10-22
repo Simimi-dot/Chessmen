94157913d35d59acec8da4f13c5477123a32a9b9
Egor Astakhov <noise718@gmail.com>
2020-10-22 12:03:28 +0300

    Initial Commit

---
+++ Chess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Егор Астахов on 22.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
